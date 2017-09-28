3adb5421f7bf3c4f9186bbe27b672fa49c98621b
itogi <32363585+itogi@users.noreply.github.com>
2017-09-28 16:42:32 +0300

    Add files via upload

---
+++ converter.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+ 
+void main()
+{
+float c,k,f;
+printf("Celsius= ");
+scanf("%f", &c); // Ввод грасусов Цельсия
+k=c+273,15;
+printf("Kelvin =%f\n", k); // Вывод грасусов Кельвина
+f=c*1.8+32;
+printf("Fahrenheit =%f\n", f); // Вывод грасусов Фаренгейт
+}
